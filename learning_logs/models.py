a018851641cbcd8808d3d1700918bbbe75d642a4
Alex Romanov <alex2012rush@gmail.com>
2021-02-05 21:09:36 +0300

    Created class to save information about learned themes.

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 
+# Create your models here.
 class Topic(models.Model):
 	"""User's theme"""
 	text = models.CharField(max_length=200)
@@ -9,4 +10,19 @@ class Topic(models.Model):
 	def __str__(self):
 		"""Returns model string description."""
 		return self.text
-# Create your models here.
+
+
+class Entry(models.Model):
+	"""Information, learned by user about theme that interests him"""
+	topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+	text = models.TextField()
+	date_added = models.DateTimeField(auto_now_add=True)
+
+
+	class Meta:
+		verbose_name_plural = 'enties'
+
+	def __str__(self):
+		"""Returns string model name."""
+		return f"{self.text[:50]}..."
+

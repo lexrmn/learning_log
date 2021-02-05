cb45cc55b81e31f266ccaf2b0356d8c7e828ac6c
Alex Romanov <alex2012rush@gmail.com>
2021-02-05 20:39:30 +0300

    Created model and added to apps in 'settings'.

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
+class Topic(models.Model):
+	"""User's theme"""
+	text = models.CharField(max_length=200)
+	date_added = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		"""Returns model string description."""
+		return self.text
 # Create your models here.
